6255f93b3b1fa1bb651c0b93b5c037123ae770a3
Pablo Rodrigo Harger <pabloharger@gmail.com>
2020-04-07 15:35:24 +0100

    'debug' heroku

--- models/index.js
+++ models/index.js
@@ -10,9 +10,13 @@ const db = {}
 
 let sequelize
 if (config.use_env_variable) {
+  console.log('1')
   sequelize = new Sequelize(process.env[config.use_env_variable], config)
+  console.log('2', process.env[config.use_env_variable])
+  console.log('3', config)
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config)
+  console.log('4', config)
 }
 
 fs
